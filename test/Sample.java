c6013990ee409fe435a047c1ae4c49626c35d08f
BALAJI P <balajibalu1015@gmail.com>
2022-01-01 13:24:09 +0530

    password functionality

---
+++ test/Sample.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Sample {
+
+	public static void password() {
+	System.out.println("password");
+
+	}
+	
+	public static void main(String[] args) {
+		password();
+		
+	}
+}
