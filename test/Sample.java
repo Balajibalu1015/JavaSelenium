63078823b1c4e002c356f725541b20f1436df04d
BALAJI P <balajibalu1015@gmail.com>
2022-01-01 13:34:20 +0530

    conflict resolved

--- test/Sample.java
+++ test/Sample.java
@@ -2,25 +2,20 @@ package test;
 
 public class Sample {
 
-<<<<<<< HEAD
-	public static void password() {
-	System.out.println("password");
-
-	}
-	
-	public static void main(String[] args) {
-		password();
-		
-	}
-=======
 	public static void username() {
 System.out.println("user");
 
 	}
 
+public static void password() {
+System.out.println("password");
+
+}
+
 public static void main(String[] args) {
 	username();
+	password();
 	
 }
->>>>>>> aa04a0ea9bc8b7f1c630966f89d7c2b592db9589
+
 }
